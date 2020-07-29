cb16b7a63d608d9c4479c323525a980fe47523e8
Hima <himat15@yahoo.com>
2020-07-29 15:10:21 -0400

    fix: update format_phone regex to match more things

--- alfred-clipboard-snippets/format_phone.sh
+++ alfred-clipboard-snippets/format_phone.sh
@@ -2,5 +2,5 @@
 
 phone_str=$(pbpaste)
 
-echo "$phone_str" | /usr/local/bin/sed -E 's/[^0-9]*([0-9]*)[^0-9]*([0-9]*)[^0-9]*([0-9]*)/\1-\2-\3/'
+echo "$phone_str" | /usr/local/bin/sed -E 's/[^0-9]*([0-9]{3})[^0-9]*([0-9]{3})[^0-9]*([0-9]{4})/\1-\2-\3/'
 # echo "$phone_str" | /usr/local/bin/sed -E 's/(\d{3}).*(\d{3}).(\d{4})/\1-\2-\3/'
